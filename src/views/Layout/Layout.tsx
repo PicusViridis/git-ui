d7c1619587b14aed6ea19aaadb4f12df2378a04c
Sara Morillon <sara.morillon@gmail.com>
2021-01-27 22:49:59 +0100

    Remove unused style files

--- src/views/Layout/Layout.tsx
+++ src/views/Layout/Layout.tsx
@@ -29,14 +29,17 @@ export default function Layout({
     <html>
       <head>
         <title>Git UI</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" />
-        <link rel="stylesheet" type="text/css" href="/styles/diff2html.min.css" />
-        <link rel="stylesheet" type="text/css" href="/styles/highlight.min.css" />
-        <link rel="stylesheet" type="text/css" href="/styles/base.css" />
         <link rel="icon" type="image/png" href="/favicon.svg" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/diff2html@3.2.0/bundles/css/diff2html.min.css" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.5.0/build/styles/default.min.css"
+        />
         <script type="text/javascript" src="/scripts/copy.js" defer></script>
         <script type="text/javascript" src="/scripts/change-branch.js" defer></script>
         <script type="text/javascript" src="/scripts/tickets.js" defer></script>
+        <style>.d2h-files-diff &#123; height: unset &#125;</style>
       </head>
       <body className="pb-5">
         <Header title={title} user={user} repo={repo} branch={branch} path={path} active={active} />
