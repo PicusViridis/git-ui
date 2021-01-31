039675cec3fae7801a1c649fa449cf1198ab4a91
Sara Morillon <sara.morillon@gmail.com>
2021-01-31 12:12:32 +0100

    Fix test case

--- tests/views/Users/Users.test.tsx
+++ tests/views/Users/Users.test.tsx
@@ -4,18 +4,18 @@ import ListUsers from '../../../src/views/Users/Users'
 import { mockUser1, mockUser2 } from '../../mocks/fixtures'
 
 describe('ListUsers', () => {
-  it('Should render each user', () => {
+  it('should render each user', () => {
     render(<ListUsers user={mockUser1} users={[mockUser1, mockUser2]} />)
     expect(screen.getByText('user1')).toBeInTheDocument()
     expect(screen.getByText('user2')).toBeInTheDocument()
   })
 
-  it('Should disabled delete if user is the same as session user', () => {
+  it('should disabled delete if user is the same as session user', () => {
     render(<ListUsers user={mockUser1} users={[mockUser1]} />)
     expect(screen.getByRole('button', { name: /Delete/i })).toBeDisabled()
   })
 
-  it('Should enable delete if user is the same as session user', () => {
+  it('should enable delete if user is the same as session user', () => {
     render(<ListUsers user={mockUser1} users={[mockUser2]} />)
     expect(screen.getByRole('link', { name: /Delete/i })).toHaveAttribute('href', '/users/delete-user/2')
   })
