c5cb19007e6f8ab6dfd7c4b8d398ae4deb262402
Sara Morillon <sara.morillon@gmail.com>
2021-02-02 20:55:59 +0100

    Fix issues release

--- src/controllers/issues/getIssue.ts
+++ src/controllers/issues/getIssue.ts
@@ -6,8 +6,30 @@ import { Release } from '../../models/Release'
 export type Req = Request<{ repo: string; id: string }>
 
 export async function getIssue(req: Req, res: Response): Promise<void> {
-  const { id, repo } = req.params
+  const { id } = req.params
+  if (id) {
+    await editIssue(req, res)
+  } else {
+    await addIssue(req, res)
+  }
+}
+
+export async function addIssue(req: Req, res: Response): Promise<void> {
+  const { repo } = req.params
   const releases = await Release.getRepository().find({ where: { repo, dueDate: MoreThan(new Date().toISOString()) } })
-  const issue = !id ? undefined : await Issue.getRepository().findOne(id, { relations: ['release'] })
+  res.render('Issues/Issue', { releases })
+}
+
+export async function editIssue(req: Req, res: Response): Promise<void> {
+  const { id, repo } = req.params
+  const issue = await Issue.getRepository().findOne(id, { relations: ['release', 'attachments'] })
+  if (!issue) {
+    return res.render('404')
+  }
+  const dueDate = MoreThan(new Date().toISOString())
+  const releases = await Release.getRepository().find({ where: { repo, dueDate } })
+  if (releases.findIndex((r) => r.id === issue.release.id) === -1) {
+    releases.push(issue.release)
+  }
   res.render('Issues/Issue', { issue, releases })
 }
