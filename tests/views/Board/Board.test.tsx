91c6d928c71bdaeba104f98b3e725abcb481246b
Sara Morillon <sara.morillon@gmail.com>
2021-02-02 09:18:42 +0100

    Fix tests

--- tests/views/Board/Board.test.tsx
+++ tests/views/Board/Board.test.tsx
@@ -22,7 +22,7 @@ describe('Board', () => {
 
   it('should render release date', () => {
     render(<Board {...props} />)
-    expect(screen.getByText('(February 1st, 2021)')).toBeInTheDocument()
+    expect(screen.getByText('(January 1st, 2020)')).toBeInTheDocument()
   })
 
   it('should render tickets', () => {
