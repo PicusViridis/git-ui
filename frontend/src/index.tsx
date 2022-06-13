ee1268bf6aee8796f97bbc14d1f9afedf72fa3db
Sara Morillon <sara.morillon.pro@gmail.com>
2022-06-13 16:52:39 +0200

    Use mini css

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,5 @@
-import 'basscss/css/basscss.min.css'
-import 'diff2html/bundles/css/diff2html.min.css'
-import 'highlight.js/styles/default.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import { App } from './ui/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
